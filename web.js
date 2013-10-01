ce7ae56a10324fa976976f42878831474d8e408f
Shary M <syed.shariyar@gmail.com>
2013-10-01 12:21:38 -0400

    Added static and compress content serving for node

--- web.js
+++ web.js
@@ -1,14 +1,63 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
-app.get('/', function(request, response) {
-    var fs = require ('fs');                        
-    var data = fs.readFileSync('index.html');  
-    var buf = new Buffer (data);                                                   //console.log(buf.toString('utf-8'));
-    response.send(buf.toString('utf-8'));
+//app.use(express.static(__dirname ));
 
-});
+//console.log(__dirname);
+// The number of milliseconds in one day
+var oneDay = 86400000;
+
+// Use compress middleware to gzip content
+app.use(express.compress());
+
+// Serve up content from public directory
+app.use(express.static(__dirname, { maxAge: oneDay }));
+
+//var app = express.createServer(express.logger());
+//
+//app.get('/', function(request, response) {
+//    var fs = require ('fs');                        
+//    var data = fs.readFileSync('index.html');  
+//    var buf = new Buffer (data);
+//    response.send(buf.toString('utf-8'));
+//    
+//});
+
+//app.get('/index.html', function(request, response) {
+//    var fs = require ('fs');                        
+//    var data = fs.readFileSync('index.html');  
+//    var buf = new Buffer (data);  
+//    response.send(buf.toString('utf-8'));
+//
+//});
+
+//app.get('/login.html', function(request, response) {
+//    var fs = require ('fs');                        
+//    var data = fs.readFileSync('login.html');  
+//    var buf = new Buffer (data);  
+//    response.send(buf.toString('utf-8'));
+//
+//});
+
+//app.get('/register.html', function(request, response) {
+//    var fs = require ('fs');                        
+//    var data = fs.readFileSync('register.html');  
+//    var buf = new Buffer (data);  
+//    response.send(buf.toString('utf-8'));
+//
+//});
+
+
+
+
+
+
+
+
+
+
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
