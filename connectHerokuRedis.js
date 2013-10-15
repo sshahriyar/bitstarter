e66d8cd5544ba1bd5b96cbd0d27775b3f22f9c95
Shary M <syed.shariyar@gmail.com>
2013-10-15 00:31:27 -0400

    hostname not host for Redis config form url.parse

--- connectHerokuRedis.js
+++ connectHerokuRedis.js
@@ -16,16 +16,16 @@ module.exports = function(express) {
     options = options || {};
 
     if (redisToGo) {
-      options.host = options.host || redisToGo.host;
+      options.host = options.host || redisToGo.hostname;
       options.port = options.port || redisToGo.port;
       
       if (!options.pass && redisToGo.auth) {
         options.pass = options.pass || redisToGo.auth.split(":")[1];
       }
     }
-   options.host="beardfish.redistogo.com";
-   options.port="10270";
-   options.pass="c9ed2893f0406ef32ef973b45dd3b08f";
+   //options.host="beardfish.redistogo.com";
+   //options.port="10270";
+   //options.pass="c9ed2893f0406ef32ef973b45dd3b08f";
       console.log("RedisStore options", options);
     RedisStore.call(this, options);
   }
